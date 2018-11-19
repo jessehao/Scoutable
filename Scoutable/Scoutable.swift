dc5d46a04c9189dd2abdb4bb9665c0fe48130742
Jesse Hao <jeasygates@hotmail.com>
2018-11-19 16:45:00 +0800

    update: add usage comments

--- Scoutable/Scoutable.swift
+++ Scoutable/Scoutable.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Snoware. All rights reserved.
 //
 
+/// Scoutable is a wrapper for weak reference which could be an item for weak array.
 struct Scoutable<T:AnyObject> {
 	weak var value:T?
 	init(_ value: T?) {
@@ -18,10 +19,18 @@ extension Scoutable : ExpressibleByNilLiteral {
 }
 
 prefix operator *
+/// Scoutable uses `*` as its instance operator. e.g. let scout = *strongRef
+///
+/// - Parameter unwrapped: unwrapped strong reference
+/// - Returns: weak reference wrapped by Scoutable
 prefix func * <T:AnyObject>(_ unwrapped:@autoclosure () -> T?) -> Scoutable<T> {
 	return Scoutable<T>(unwrapped())
 }
 
+/// You can write code like `let scout = *Object?.self` to create an empty Scoutable weak wrapper quickly.
+///
+/// - Parameter meta: specific metatype
+/// - Returns: empty weak reference wrapper
 prefix func * <T:AnyObject>(_ meta:T?.Type) -> Scoutable<T> {
 	return Scoutable<T>(nilLiteral: ())
 }
