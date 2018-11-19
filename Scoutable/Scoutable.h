2e517623b38f0969a4bd7c40796eb3cd242894df
Jesse Hao <jeasygates@hotmail.com>
2018-11-19 15:10:13 +0800

    Initial Commit

---
+++ Scoutable/Scoutable.h
@@ -0,0 +1,19 @@
+//
+//  Scoutable.h
+//  Scoutable
+//
+//  Created by Jesse Hao on 2018/11/19.
+//  Copyright © 2018 Snoware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Scoutable.
+FOUNDATION_EXPORT double ScoutableVersionNumber;
+
+//! Project version string for Scoutable.
+FOUNDATION_EXPORT const unsigned char ScoutableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scoutable/PublicHeader.h>
+
+
